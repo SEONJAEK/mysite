351652dedba3812bc020efc4823d0bbd02eff355
bit <bit@DESKTOP-N5AI91N>
2022-01-14 16:09:42 +0900

    mysite02 #05

--- mysite02/src/main/java/com/poscoict/mysite/mvc/user/UserActionFactory.java
+++ mysite02/src/main/java/com/poscoict/mysite/mvc/user/UserActionFactory.java
@@ -1,5 +1,6 @@
 package com.poscoict.mysite.mvc.user;
 
+import com.poscoict.mysite.mvc.main.MainAction;
 import com.poscoict.web.mvc.Action;
 import com.poscoict.web.mvc.ActionFactory;
 
@@ -7,8 +8,15 @@ public class UserActionFactory extends ActionFactory {
 
 	@Override
 	public Action getAction(String actionName) {
+		Action action = null;
 		
-		return null;
+		if("joinform".equals(actionName)) {
+			action = new JoinFormAction();
+		} else {
+			//장난치는 경우
+			action = new MainAction();
+		}
+		return action;
 	}
 
 }
