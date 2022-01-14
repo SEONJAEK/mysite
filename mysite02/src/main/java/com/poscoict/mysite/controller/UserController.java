b5299e2c3d58f340c3064047648b3beee3cc59a7
bit <bit@DESKTOP-N5AI91N>
2022-01-14 15:42:33 +0900

    mysite02 #04

--- mysite02/src/main/java/com/poscoict/mysite/controller/UserController.java
+++ mysite02/src/main/java/com/poscoict/mysite/controller/UserController.java
@@ -6,13 +6,12 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-
 import com.poscoict.mysite.mvc.user.UserActionFactory;
 import com.poscoict.web.mvc.Action;
 import com.poscoict.web.mvc.ActionFactory;
 
 public class UserController extends HttpServlet {
-	private static final long serialVersionUID = 1L;
+	private static final long serialVersionUID = 1L; 
 
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		request.setCharacterEncoding("utf-8");
