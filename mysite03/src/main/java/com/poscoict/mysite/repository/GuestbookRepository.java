e1467363a53c213ea76bb228f195eb801f01758f
bit <bit@DESKTOP-N5AI91N>
2022-01-25 19:47:19 +0900

    mybatis로 수정하기

--- mysite03/src/main/java/com/poscoict/mysite/repository/GuestbookRepository.java
+++ mysite03/src/main/java/com/poscoict/mysite/repository/GuestbookRepository.java
@@ -1,13 +1,7 @@
 package com.poscoict.mysite.repository;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.util.ArrayList;
-import java.util.List;
 
+import java.util.List;
 import org.apache.ibatis.session.SqlSession;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
@@ -33,56 +27,10 @@ public class GuestbookRepository {
 	}	
 
 	public int delete(GuestbookVo vo) {
-		int result = 0;
-
-		Connection conn = null;
-		PreparedStatement pstmt = null;
-		try {
-			conn = getConnection();
-			
-			String sql =
-					" delete" +
-					"   from guestbook" +
-					"  where no=?" +
-					"    and password=?";
-			pstmt = conn.prepareStatement(sql);
-			
-			pstmt.setLong(1, vo.getNo());
-			pstmt.setString(2, vo.getPassword());
-			
-			result = pstmt.executeUpdate();
-		} catch (SQLException e) {
-			System.out.println("error:" + e);
-		} finally {
-			try {
-				if(pstmt != null) {
-					pstmt.close();
-				}
-				if(conn != null) {
-					conn.close();
-				}
-			} catch (SQLException e) {
-				e.printStackTrace();
-			}
-		}		
-		
-		return result;		
+		return sqlSession.delete("guestbook.delete");
 	}
 	
 	public int insert(GuestbookVo vo) {
 		return sqlSession.insert("guestbook.insert", vo);
-	}
-	
-	private Connection getConnection() throws SQLException {
-		Connection conn = null;
-		try {
-			Class.forName("com.mysql.cj.jdbc.Driver");
-			String url = "jdbc:mysql://localhost:3306/webdb?characterEncoding=UTF-8&serverTimezone=UTC";
-			conn = DriverManager.getConnection(url, "webdb", "webdb");
-		} catch (ClassNotFoundException e) {
-			System.out.println("드라이버 로딩 실패:" + e);
-		} 
-		
-		return conn;
 	}
 }
