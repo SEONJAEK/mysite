bc0478c5544dda4d20130501b76dd3b1999fa48e
bit <bit@DESKTOP-N5AI91N>
2022-01-26 12:37:38 +0900

    mysite03 - logback(slf4j) 적용

--- mysite03/src/main/java/com/poscoict/mysite/exception/GlobalExceptionHandler.java
+++ mysite03/src/main/java/com/poscoict/mysite/exception/GlobalExceptionHandler.java
@@ -3,6 +3,8 @@ package com.poscoict.mysite.exception;
 import java.io.PrintWriter;
 import java.io.StringWriter;
 
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -10,15 +12,17 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 //advice가 붙으면 what에 해당 aop라는 것을 알 수 있다. 
 @ControllerAdvice
 public class GlobalExceptionHandler {
+	private static final Log LOGGER = LogFactory.getLog(GlobalExceptionHandler.class);
 	
 	@ExceptionHandler(Exception.class)
 	public String ExceptionHandler(Model model, Exception e) {
 		//1. 로깅
 		//String errors = e.toString();
-		
 		StringWriter errors = new StringWriter();
 		e.printStackTrace(new PrintWriter(errors));
-		System.out.println(errors.toString());
+		//화면에 남기지 않고 로그를 사용해서 뿌리겠다
+		//System.out.println(errors.toString());
+		LOGGER.error(errors.toString());
 		
 		
 		//2. 사과페이지(HTML응답, 정상 종료)
