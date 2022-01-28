d5bb9b4f9ab087737b104bae727dc95aad8d2891
bit <bit@DESKTOP-N5AI91N>
2022-01-28 18:53:21 +0900

    //mysite 수정

--- mysite02/src/main/java/com/poscoict/mysite/mvc/user/UpdateAction.java
+++ mysite02/src/main/java/com/poscoict/mysite/mvc/user/UpdateAction.java
@@ -7,7 +7,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
-import com.mysql.cj.Session;
+
 import com.poscoict.mysite.dao.UserDao;
 import com.poscoict.mysite.vo.UserVo;
 import com.poscoict.web.mvc.Action;
