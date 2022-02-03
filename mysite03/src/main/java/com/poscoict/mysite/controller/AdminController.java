9063b86992d06d932b8f345d8246999c08506b2a
bit <bit@DESKTOP-N5AI91N>
2022-02-03 16:18:36 +0900

    mysite03 #finish

--- mysite03/src/main/java/com/poscoict/mysite/controller/AdminController.java
+++ mysite03/src/main/java/com/poscoict/mysite/controller/AdminController.java
@@ -7,26 +7,28 @@ import org.springframework.web.bind.annotation.ResponseBody;
 import com.poscoict.mysite.security.Auth;
 
 //이 컨트롤러에 있는 모든 핸들러(메소드)는 다 인증을 받아라라는 뜻
-@Auth
+@Auth(role="ADMIN")
 @Controller
 @RequestMapping("/admin")
 public class AdminController {
 	
-	@ResponseBody
-	@RequestMapping("/main")
+	@RequestMapping("")
 	public String main() {
-		return "AdminController/main()";
-	}
-	
-	@ResponseBody
-	@RequestMapping("/board")
-	public String board() {
-		return "AdminController.board()";
+		return "admin/main";
 	}
 
-	@ResponseBody
+	@RequestMapping("/board")
+	public String board() {
+		return "admin/board";
+	}
+
 	@RequestMapping("/guestbook")
 	public String guestbook() {
-		return "AdminController.guestbook()";
+		return "admin/guestbook";
 	}
+
+	@RequestMapping("/user")
+	public String user() {
+		return "admin/user";
+	}	
 }
