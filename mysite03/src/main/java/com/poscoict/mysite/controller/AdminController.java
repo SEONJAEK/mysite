9b3320b738a03d159c8430f102471394402d630b
bit <bit@DESKTOP-N5AI91N>
2022-02-03 16:55:23 +0900

    mysite03 #s1

--- mysite03/src/main/java/com/poscoict/mysite/controller/AdminController.java
+++ mysite03/src/main/java/com/poscoict/mysite/controller/AdminController.java
@@ -1,21 +1,41 @@
 package com.poscoict.mysite.controller;
 
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import javax.servlet.ServletContext;
 
-import com.poscoict.mysite.security.Auth;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MultipartFile;
+
+import com.poscoict.mysite.service.SiteService;
+import com.poscoict.mysite.vo.SiteVo;
 
 //이 컨트롤러에 있는 모든 핸들러(메소드)는 다 인증을 받아라라는 뜻
-@Auth(role="ADMIN")
+//@Auth(role="ADMIN")
 @Controller
 @RequestMapping("/admin")
 public class AdminController {
+	@Autowired
+	private SiteService siteService;
 	
-	@RequestMapping("")
-	public String main() {
+//	@Autowired
+//	private ServletContext servletContext;
+
+	@RequestMapping(value = "")
+	public String main(Model model) {
+		model.addAttribute("sitevo", siteService.selectAll());
 		return "admin/main";
 	}
+
+//	@RequestMapping(value = "/update", method = RequestMethod.POST)
+//	public String update(@RequestParam(value = "upload-file") MultipartFile multipartFile, SiteVo siteVo) {
+//		System.out.println("========================" + siteVo);
+//		siteService.update(siteVo);
+//		return "redirect:/main/index";
+//	}
 
 	@RequestMapping("/board")
 	public String board() {
@@ -30,5 +50,5 @@ public class AdminController {
 	@RequestMapping("/user")
 	public String user() {
 		return "admin/user";
-	}	
+	}
 }
