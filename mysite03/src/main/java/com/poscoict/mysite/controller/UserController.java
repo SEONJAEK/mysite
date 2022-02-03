82dafb9988e7683295a704879eaa4f2d0cbe3365
bit <bit@DESKTOP-N5AI91N>
2022-02-03 10:47:09 +0900

    validation #2

--- mysite03/src/main/java/com/poscoict/mysite/controller/UserController.java
+++ mysite03/src/main/java/com/poscoict/mysite/controller/UserController.java
@@ -11,6 +11,7 @@ import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.validation.ObjectError;
 import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
@@ -32,17 +33,19 @@ public class UserController {
 	
 	//그 결과를 알려줘 바인딩해줘, 제대로 받은 데이터랑 너가 세운 조건이랑 맞는지 바인딩하구 그 결과
 	@RequestMapping(value="/join", method=RequestMethod.POST)
-	public String join(@Valid UserVo userVo, BindingResult result, Model model) {
+	public String join(@ModelAttribute @Valid UserVo vo, BindingResult result, Model model) {
 		if(result.hasErrors()) {
 //			List<ObjectError> list = result.getAllErrors();
 //			for(ObjectError error : list) {
 //				System.out.println(error);
 //			}
+			
+			//@ModelAttribute 사용시, 의미는 @Valid의 UserVo의 소문자 userVo로 만들어준것
+			//model.addAttribute("userVo", vo);
 			model.addAllAttributes(result.getModel());
 			return "user/join";
 		}
-		
-		userService.join(userVo);
+		userService.join(vo);
 		return "redirect:/user/joinsuccess";
 	}
 	
