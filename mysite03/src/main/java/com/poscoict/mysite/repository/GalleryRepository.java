4a860ccc912496004cc604e883229e2d0ae73385
SEONJAEK <nalalasjsj@gmail.com>
2022-02-18 20:51:58 +0900

    mysite03 css#01

---
+++ mysite03/src/main/java/com/poscoict/mysite/repository/GalleryRepository.java
@@ -0,0 +1,31 @@
+package com.poscoict.mysite.repository;
+
+import java.util.List;
+
+import org.apache.ibatis.session.SqlSession;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
+import com.poscoict.mysite.vo.GalleryVo;
+
+@Repository
+public class GalleryRepository {
+	
+	@Autowired
+	private SqlSession sqlSession;
+
+	public List<GalleryVo> findAll() {
+		return sqlSession.selectList("gallery.findAll");
+	}
+
+	public int delete(Long no) {
+		return sqlSession.delete("gallery.delete");
+	}
+
+	public int insert(GalleryVo vo) {
+		return sqlSession.insert("gallery.insert", vo);
+	}
+	
+	
+	
+}
---
+++ mysite03/src/main/java/com/poscoict/mysite/service/GalleryService.java
@@ -0,0 +1,28 @@
+package com.poscoict.mysite.service;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.poscoict.mysite.repository.GalleryRepository;
+import com.poscoict.mysite.vo.GalleryVo;
+
+@Service
+public class GalleryService {
+	@Autowired
+	private GalleryRepository galleryRepository;
+
+	public List<GalleryVo> getImages() {
+		return galleryRepository.findAll();
+	}
+	
+	public Boolean removeImge(Long no) {
+		return galleryRepository.delete(no) == 1;
+	}
+	
+	public Boolean saveImage(GalleryVo vo) {
+		return galleryRepository.insert(vo) == 1;
+	}
+	
+}
